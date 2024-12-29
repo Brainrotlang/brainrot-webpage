751bf5a9c92b580e7b13ed4631061dfd27a68ced
Leonardo Araujo <leonardo.aa88@gmail.com>
2024-12-29 03:24:39 -0300

    fix: discord links

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,7 +7,7 @@ return (
             <div className="container mx-auto px-4 text-center">
             <p className="mb-4">Made with 💀 by the most unhinged developers 🧟</p>
             <div className="flex justify-center space-x-6">
-                <a href="https://discord.com/channels/1321786173349298176/1321786173781184532" className="hover:text-purple-400">Discord</a>
+                <a href="https://discord.gg/G9BqwB3a" className="hover:text-purple-400">Discord</a>
                 <a href="https://github.com/Brainrotlang/brainrot" className="hover:text-purple-400">GitHub</a>
             </div>
             <p className="mt-6 text-sm text-gray-400">
