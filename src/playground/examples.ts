f2645f5d26260dda6b275abce45aafb263d5c0c5
Leonardo <46436462+leo-aa88@users.noreply.github.com>
2026-08-20 20:54:57 -0300

    Refactor slorp usage to store result in variable

--- src/playground/examples.ts
+++ src/playground/examples.ts
@@ -55,8 +55,8 @@ export const PLAYGROUND_EXAMPLES: PlaygroundExample[] = [
     source: `skibidi main {
     yap name[32];
     yapping("Yo, what's the name, chief?");
-    slorp(name);
-    yapping("%s? Certified W rizz fr fr.", name);
+    rant string = slorp(name);
+    yapping("%s? Certified W rizz fr fr.", string);
     bussin 0;
 }
 `,
